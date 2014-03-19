dda5dec2136fe3399527bfe78debe64901c04132
Kevin Gaddy <kevin.gaddy@gmail.com>
2014-03-19 18:29:48 -0500

    Upgraded to node v0.10.24

--- MoxJSON.js
+++ MoxJSON.js
@@ -4,16 +4,19 @@ var url = require('url');
 var mockData = require('./mockData.js');
 var svcLog = require('./serviceLog.js');
 var errorHandler = require('./error.js');
+var log = require('./logger.js');
 var server = http.createServer(function(request, response) {
 	var urlObj = url.parse(request.url, true);
 	var id = urlObj.query.id;
 	var output = urlObj.query.output;
 	//var user = 'kgaddy';
-	request.addListener('end',
+	request.on('end',
 
-	function() {
-		var serviceid = id;
-		mockData.getData(serviceid, response, request, output);
-	});
+		function() {
+			var serviceid = id;
+			log.logger(id, 0);
+			mockData.getData(serviceid, response, request, output);
+		});
+	request.resume();
 }).listen(8000);
 console.log('Server Started on port:8000')
